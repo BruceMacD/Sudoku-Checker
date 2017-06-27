bb63e81b7d1fe9eb591d56b6175f004ace8f5ffb
BruceMacD <brucewmacdonald@gmail.com>
2017-06-27 19:39:28 -0300

    project structure

---
+++ cmake-build-debug/CMakeFiles/Sudoku_Checker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sudoku_Checker.dir/main.c.o"
+  "Sudoku_Checker.pdb"
+  "Sudoku_Checker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sudoku_Checker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
